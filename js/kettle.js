3d27974823d0458a5bde9fb817694a29c2682909
Rena Hashimi <renhas54143@stud.noroff.no>
2023-09-20 01:14:10 +0200

    Update js - new

--- js/kettle.js
+++ js/kettle.js
@@ -1,16 +1,6 @@
+// DESCRIPTION OF MY KETTLE
 
-// My kettle
-
-var object = "Kettle";
-var name = "Bosch";
-var item = object +"-"+ name;
-
-console.log(item);
-
-
-// Description of my kettle
-
-const kettle = {
+var kettle = {
     brand: "Bosch",
     model: "TWK7203",
     capasity: 1.7,
@@ -24,29 +14,63 @@ const kettle = {
     isItOff: true,
     lowDegree: 0,
     highDegree: 100,
-    setTemprature: [70, 75, 80, 85, 90, 95, 100]
+    setTemperture: [70, 75, 80, 85, 90, 95, 100]
+}
+console.log(kettle);
+
+
+// STRING
+
+var object = "Kettle";
+var name = "Bosch";
+var item = object +"-"+ name;
+
+console.log(item);
+
+// ARRAY & NUMBERS
+
+var temperture = [70, 75, 80, 85, 90, 95, 100];
+console.log (temperture);
+
+
+// Available boiling points / tempertures in a list
+
+for(var i = 0; i < 7; i++){
+
+    // console.log([i]);
+    var number = temperture[i];
+
+    console.log(number);
 }
 
-console.log(kettle)
 
 
-// Buttons on my kettle
-  
-var button = "off";
+// FUNCTION
 
-if(button === "on") {
-    console.log ("Kettle is boiling");
-}   else {
-        console.log("Kettle is not boiling")
-}
+// lag en knapp
 
-// Water temprature
-var setTemprature = 70; //Perfect water temprature
 
-if(setTemprature === 80){
+
+// BOOLEANS
+
+
+
+
+// Water temperture
+var setTemperture = 80; //Perfect water temperture
+
+if(setTemperture === 80){
     console.log ("Water is perfect")
-}   else { (setTemprature !== 80)
+}   else { (setTemperture !== 80)
     console.log ("Water is too cold or too hot!")
 }
 
 
+// Buttons on my kettle
+
+var button = "off";
+if(button === "on") {
+    console.log ("Kettle is boiling");
+}   else { 
+    console.log ("Kettle is not boiling")
+}
