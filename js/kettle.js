ec91c55ef3b66974ada04cf50aafceb2c13aa3a2
Rena Hashimi <renhas54143@stud.noroff.no>
2023-09-22 01:00:46 +0200

    Update changes and addings CSS, Index, JS

--- js/kettle.js
+++ js/kettle.js
@@ -1,6 +1,6 @@
 // DESCRIPTION OF MY KETTLE
 
-var kettle = {
+const kettle = {
     brand: "Bosch",
     model: "TWK7203",
     capasity: 1.7,
@@ -10,32 +10,33 @@ var kettle = {
     watt: 2200,
     weight: 1.827,
     price: 1050,
-    isItOn: false,
-    isItOff: true,
-    lowDegree: 0,
-    highDegree: 100,
+    maxcelsius: 100,
     setTemperture: [70, 75, 80, 85, 90, 95, 100]
 }
 console.log(kettle);
 
 
-// STRING
 
-var object = "Kettle";
-var name = "Bosch";
+
+// String
+
+const object = "Kettle";
+const name = "Bosch";
 var item = object +"-"+ name;
 
 console.log(item);
 
-// ARRAY & NUMBERS
+
+
+
+// Array & Numbers
 
 var temperture = [70, 75, 80, 85, 90, 95, 100];
 console.log (temperture);
 
-
 // Available boiling points / tempertures in a list
 
-for(var i = 0; i < 7; i++){
+for (var i = 0; i < 7; i++){
 
     // console.log([i]);
     var number = temperture[i];
@@ -45,18 +46,22 @@ for(var i = 0; i < 7; i++){
 
 
 
-// FUNCTION
 
-// lag en knapp
+// Booleans
 
-
-
-// BOOLEANS
+var brandName = ("Bosch");
+if (brandName) {
+    console.log (true);
+} if (!brandName) {
+    console.log (false);
+}
+    
 
 
 
 
 // Water temperture
+
 var setTemperture = 80; //Perfect water temperture
 
 if(setTemperture === 80){
@@ -66,7 +71,9 @@ if(setTemperture === 80){
 }
 
 
-// Buttons on my kettle
+
+
+// On/Off button on my kettle
 
 var button = "off";
 if(button === "on") {
@@ -74,3 +81,71 @@ if(button === "on") {
 }   else { 
     console.log ("Kettle is not boiling")
 }
+
+
+
+
+
+// Function
+
+console.log (kettle.model); // TWK7203
+console.log (kettle.capasity); // 1.7 liter
+
+
+
+function boilSomeWater() {
+    console.log ("Fill the kettle with water");
+    console.log ("Push the ON button");
+    console.log ("Water is boiling");
+    console.log ("Water is boiled and kettle is OFF");
+}
+
+boilSomeWater();
+
+// Function 2
+
+function LetsBoilSomeMoreWater() {
+}
+    let fill = "Fill the kettle once more with water";
+    let pushButton = "and push the ON button again.";
+    let boiling = "Water is boiling fast and";
+    let boiled = "finally the water is boiled";  
+    let enjoy = "for you to enjoy a second cup of tea!";
+
+    result = (fill+" "+pushButton+" "+boiling+" "+boiled+" "+enjoy);
+    
+    console.log (result);
+
+
+// Button ON
+
+
+function change(){
+    document.getElementById("text").innerHTML = "ON";
+    document.getElementById("btn").style.background = "rgb(0, 128, 0)";   
+}
+
+console.log (change);
+
+
+
+
+/*else (!change) {
+    document.getElementById("text").innerHTML = "OFF";
+    document.getElementById("btn").style.background = "rgb(255, 0, 0)";
+}
+}
+
+
+/*
+    document.getElementById("text").innerHTML = "OFF";
+    document.getElementById("btn").style.background = "rgb(255, 0, 0)";
+*/
+
+
+
+
+
+
+
+
